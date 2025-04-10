3675befc25680aef9beff6a6895e0344854b32ad
Kevin Gasga <157638838+sigmamonster69@users.noreply.github.com>
2025-04-10 16:21:38 -0500

    Add files via upload

---
+++ homework.rs
@@ -0,0 +1,43 @@
+enum homeworkassignment{
+
+hw1,
+hw2,
+hw3,
+
+
+}
+struct classes{
+
+class: string
+assignment: string
+due date: usize
+}
+
+fn main() {
+
+let hw = classes {
+name: String::from("Kevin's class he teaches maybe")
+due1: 13,
+due2: 18,
+due3: 26,
+};
+let my_classes = homeworkassignment::hw1;
+let my_classes1 = homeworkassignment::hw2;
+let my_classes2 = homeworkassignment::hw3;
+println!("Hello, I am kevin the professor cool guy and your {:?} {:?
+}{:?} are due on june {}",classes.due,my_classes);
+
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+                                                  29,1          All
